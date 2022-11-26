c022481dad6e4d7c773ce4f59c24cf708e4d60a9
adi_random <me.adi.hf@gmail.com>
2022-11-26 11:56:57 +0200

    Add change bullet animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,15 +9,24 @@ public class Player : MonoBehaviour
 {
     [field: SerializeField] public int CurrentHealth { get; private set; } = 4;
     [field: SerializeField] public int MaxHealth { get; private set; } = 4;
+
     [field: SerializeField]
     public List<BulletType>
         AvailableBullets { get; private set; } = new();
 
-    [field: SerializeField]
-    public BulletType CurrentBullet { get; set; }
+    [field: SerializeField] public BulletType CurrentBullet { get; set; }
+    [field: SerializeField] public bool CanShoot { get; set; }
 
     [SerializeField] private HudManager ui;
-    
+
+    private ProjectileOrbitalController _orbitalController;
+    private Coroutine _changeBulletCoroutine = null;
+
+    private void Start()
+    {
+        _orbitalController = GetComponent<ProjectileOrbitalController>();
+    }
+
     private void Update()
     {
         CheckStatus();
@@ -44,7 +53,7 @@ public class Player : MonoBehaviour
             TakeDamage(1);
         }
     }
-    
+
     private void CheckStatus()
     {
         if (CurrentHealth <= 0)
@@ -52,5 +61,21 @@ public class Player : MonoBehaviour
             Destroy(gameObject);
             SceneManager.LoadScene(0);
         }
+    }
+
+    public void SetBullet(BulletType bulletType)
+    {
+        this.CurrentBullet = bulletType;
+        this.CanShoot = false;
+        if (_changeBulletCoroutine != null)
+        {
+            StopCoroutine(_changeBulletCoroutine);
+        }
+
+        _changeBulletCoroutine =
+            StartCoroutine(_orbitalController.ChangeBullet(
+                bulletType,
+                () => this.CanShoot = true)
+            );
     }
 }
